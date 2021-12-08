d13162efc4197e6693e498230d1839eeec60b0e9
Evgeny Izutov <evgeny.izutov@intel.com>
2021-12-08 16:20:11 +0300

    Minor

--- configs/litehrnet/fcn_litehr18_split_512x512_40k_kvasir-seg.py
+++ configs/litehrnet/fcn_litehr18_split_512x512_40k_kvasir-seg.py
@@ -1,16 +1,16 @@
 _base_ = [
-    '../_base_/models/fcn_litehr18.py', '../_base_/datasets/kvasir.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_cos_40k.py'
+    '../_base_/models/fcn_litehr18_no-aggregator.py', '../_base_/datasets/kvasir.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_step_40k_ml_adam.py'
 ]
 
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 model = dict(
     decode_head=dict(
         type='FCNHead',
-        in_channels=40,
-        in_index=0,
+        in_channels=[40, 80, 160, 320],
+        in_index=[0, 1, 2, 3],
+        input_transform='multiple_select',
         channels=40,
-        input_transform=None,
         kernel_size=1,
         num_convs=0,
         concat_input=False,
@@ -18,6 +18,8 @@ model = dict(
         num_classes=2,
         norm_cfg=norm_cfg,
         align_corners=False,
+        enable_aggregator=True,
+        enable_out_norm=False,
         loss_decode=[
             dict(
                 type='CrossEntropyLoss',
